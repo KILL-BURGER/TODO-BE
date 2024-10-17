eb2d8d2228a19b841d539bb3ae5c098f03faceb5
이동주 <idongju@idongjuui-MacBookPro.local>
2024-10-18 00:07:46 +0900

    setting

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
+const mongoURI = `mongodb://localhost:27017/todo-demo`;
+mongoose.connect(mongoURI, {useNewUrlParser: true})
+    .then(() => {
+        console.log('mongoose connected')})
+    .catch((err) => {
+        console.log("DB connection fail", err);
+});
 
 app.listen(5050, () => {
     console.log('5050 server start..!');
-})
+});
